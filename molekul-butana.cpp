d7037eaa16de8bdda77175e166583e0827d92e25
ayuauliadevi <106718691+ayuauliadevi@users.noreply.github.com>
2022-06-04 08:36:22 +0700

    Update molekul-butana.cpp
    Line 13 - 27 butana terdiri dari 4 atom C dan 10 atom H

--- molekul-butana.cpp
+++ molekul-butana.cpp
@@ -9,3 +9,19 @@ using namespace std;
 int main(){
     // deklarasi variabel
     float pi = 3.14;
+  
+    // deklarasi molekul butana
+    float C1x, C1y, C1z;
+    float C2x, C2y, C2z;
+    float C3x, C3y, C3z;
+    float C4x, C4y, C4z;
+    float H1x, H1y, H1z;
+    float H2x, H2y, H2z;
+    float H3x, H3y, H3z;
+    float H4x, H4y, H4z;
+    float H5x, H5y, H5z;
+    float H6x, H6y, H6z;
+    float H7x, H7y, H7z;
+    float H8x, H8y, H8z;
+    float H9x, H9y, H9z;
+    float H10x, H10y, H10z;
