fa51191be98e22cce45f1a030b44fcd9307c7f27
Navindaramadhan <106718765+Navindaramadhan@users.noreply.github.com>
2022-06-03 21:37:39 -0700

    Update molekul-butana.cpp
    Penambahan variabel keempat karbon.
    Panjang antar ikatan karbon ialah C-C = 1.540 A
    Panjang ikatan C-H = 1.090 A
    Sudut antara C-C-C = 109,471 derajat
    Semua Carbon berada pada sumbu x dan z sehingga sumbu y = 0.0

--- molekul-butana.cpp
+++ molekul-butana.cpp
@@ -25,3 +25,21 @@ int main(){
     float H8x, H8y, H8z;
     float H9x, H9y, H9z;
     float H10x, H10y, H10z;
+    
+    // masukan nilai untuk setiap variabel C4H10
+
+    C1x = - sin (109.471*pi/180) * 3.7545;
+    C1y = 0.0;
+    C1z = cos (109.471*pi/180) * 3.7545;
+    
+    C2x = - cos (35.2645*pi/180) * 0.77;
+    C2y = 0.0;
+    C2z = - sin (35.2645*pi/180) * 0.77;
+
+    C3x = - C2x;
+    C3y = 0.0;
+    C3z = - C2z
+
+    C4x = - C1x
+    C4y = 0.0;
+    C4z = - C1z
